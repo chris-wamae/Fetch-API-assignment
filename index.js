7c9c4beec1d64997b41cfc48e75aef2315295814
chris-wamae <chriswamae123@gmail.com>
2022-12-07 11:43:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
-// ARROW FUNCTIONS
+// // ARROW FUNCTIONS
 
-function addTwoNumbers(a, b) {
-  return a + b;
-}
+// function addTwoNumbers(a, b) {
+//   return a + b;
+// }
 
-// let total = addTwoNumbers(10, 20);
+// // let total = addTwoNumbers(10, 20);
 
-let total = (a, b) => {
-  return a + b;
-};
+// let total = (a, b) => {
+//   return a + b;
+// };
 
-// callback functions
-let isDivisible = (x) => {
-  if (x % 2 === 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
+// // callback functions
+// let isDivisible = (x) => {
+//   if (x % 2 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
 
-let myTotal = total(50, 60); // 110
-let result = isDivisible(myTotal); //true
-// console.log(result);
+// let myTotal = total(50, 60); // 110
+// let result = isDivisible(myTotal); //true
+// // console.log(result);
 
 // FETCH API - simple function that enables us to make requests for
 // create a dynamic function that will be used to purely create element
 // and add content to the elements
 let createElementFunc = (element, content) => {
     console.log("Adding an element and its content")
+    let createdElement = document.createElement(element)
+    createdElement.textContent = content
+    return createdElement
 }
 // create a function to append the added element to a desired destination
 let appendElementFunc = (element) => {
     console.log("add the element to the desired location")
+    document.querySelector("body").append(element)
 }
 
 
-let baseURL = "https://jsonplaceholder.typicode.com";
+
+ let baseURL = "https://jsonplaceholder.typicode.com";
 let postsEndpoint = "/posts";
 
 // post url [baseurl + endpoint]
@@ -48,12 +53,8 @@ fetch(url)
     let posts = response
 
     for (post of posts){
-        console.log(post.title);
-    }
-    // add a list of this posts to the html page
-    // DOM MANIPULATION
-
-    createElementFunc()
-    appendElementFunc()
-
-  });
+        console.log(post.title)
+        appendElementFunc(createElementFunc("li",post.title))
+        ;
+        
+    } });
